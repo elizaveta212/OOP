7c86256d15c2bc4aba6ca400a635c61cf641af8a
elizaveta212 <116644834+elizaveta212@users.noreply.github.com>
2024-02-21 14:59:19 +0300

    Add files via upload

---
+++ func.h
@@ -0,0 +1,12 @@
+#ifndef FUNC_H_INCLUDED
+#define FUNC_H_INCLUDED
+
+#include <string>
+std::string foo(std::string stroka);
+
+#endif
+
+
+
+
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "func.h"
+using namespace std;
+
+
+int main(){
+    string stroka;
+    string stroka2;
+    std::cin >> stroka;
+    stroka2 = foo(stroka);
+    std::cout << stroka2 << std::endl;
+    return 0;
+}
